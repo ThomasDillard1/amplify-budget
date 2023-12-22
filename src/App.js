6cb11989366bfd93349e5d4ff352b155fc0a1a9a
Thomas <thomasdillard1@gmail.com>
2023-12-21 23:32:26 -0500

    refactor for an expense component

--- src/App.js
+++ src/App.js
@@ -15,66 +15,22 @@ import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
 
 import { useEffect, useState } from 'react';
+import ExpenseComponent from './ExpenseComponent';
 
 // To connect with amplify and configure user
 Amplify.configure(config);
 
-// To connect with the GraphQL api
-const client = generateClient();
+
 
 function App({ signOut, user }) {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    async function createExpenseItem() {
-      try {
-        const expenseInput = {
-          amount: 50.00,        // Replace with the actual amount
-          category: 'Groceries', // Replace with the actual category
-          description: 'My first expense!',
-          date: '2023-01-01'     // Replace with the actual date
-        };
-        await client.graphql({
-          query: createExpense,
-          variables: {
-            input:
-              expenseInput
-          }
-        });
-        console.log(expenseInput)
-      } catch (e){
-        console.log(e)
-      }
-    }
-    createExpenseItem();
-
-    async function displayExpenses(){
-      const expenses = await client.graphql({query:listExpenses});
-      console.log(expenses);
-      setItems(expenses.data.listExpenses.items);
-    }
-    displayExpenses();
-  }, []);
-
   return (
     <>
       <h1>Hello {user.username}</h1>
       
       <button onClick={signOut}>Sign out</button>
-      {items.map((item, index) => (
-        <div key={index}>
-          {item.description} : {index}
-        </div>
-      ))}
+      <ExpenseComponent></ExpenseComponent>
     </>
   );
 }
 
 export default withAuthenticator(App);
-
-/*
-      {items.map((item, index) => (
-        <div key={index}>
-          {item.name} : {index}
-        </div>
-      ))}
-      */
---
+++ src/ExpenseComponent.js
@@ -0,0 +1,63 @@
+/*
+    Three parts: Listing the expenses, creating the expenses, deleting the expenses
+    1) Listing the expenses - easy, do first
+    2) Creating the expenses - use a button
+    3) Deleting the expenses - use a button
+*/
+// Expenses Model
+import { createExpense, updateExpense, deleteExpense } from './graphql/mutations';
+import { listExpenses } from './graphql/queries';
+
+
+// GraphQL API
+import { generateClient } from 'aws-amplify/api';
+
+import { useEffect, useState } from 'react';
+
+// To connect with the GraphQL api
+const client = generateClient();
+
+export default function ExpenseComponent() {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+      async function createExpenseItem() {
+        try {
+          const expenseInput = {
+            amount: 50.00,        // Replace with the actual amount
+            category: 'Groceries', // Replace with the actual category
+            description: 'My first expense!',
+            date: '2023-01-01'     // Replace with the actual date
+          };
+          await client.graphql({
+            query: createExpense,
+            variables: {
+              input:
+                expenseInput
+            }
+          });
+          console.log(expenseInput)
+        } catch (e){
+          console.log(e)
+        }
+      }
+      createExpenseItem();
+  
+      async function displayExpenses(){
+        const expenses = await client.graphql({query:listExpenses});
+        console.log(expenses);
+        setItems(expenses.data.listExpenses.items);
+      }
+      displayExpenses();
+    }, []);
+
+    return (
+        <>
+        <p>I am the expense component</p>
+        {items.map((item, index) => (
+            <div key={index}>
+              {item.description} : {index}
+            </div>
+        ))}
+        </>
+    );
+}
